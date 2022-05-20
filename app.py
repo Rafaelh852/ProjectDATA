9d7d1fc7df5728ed00570d6037d6e8eff11ca2a5
Rafael Hernandez <Rafael.Hernandez852@gmail.com>
2022-05-19 21:00:24 -0700

    simple api to get data

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
+import pandas as pd
 
 app = Flask(__name__)
 
+#this is accomplished with nginx so comment out if published
 @app.route("/")
 def index():
-    return{"Root Message":"MOSHI MOSHI!"}
+    return render_template("index.html")
+
+
+@app.route("/api/<post>")
+def post_route(post):
+    path = "./data"
+    sub = "/"
+    dbname = post
+    dbpath = path + sub + dbname
+    dbfile = os.listdir(dbpath)
+    return pd.read_csv(dbpath + sub + dbfile[0]).to_json()
 
 
 if __name__ == '__main__':
